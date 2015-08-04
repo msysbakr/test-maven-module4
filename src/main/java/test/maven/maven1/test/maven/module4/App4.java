21d336d37ab26921a55e345621246b9149842d5b
abc <medisysbd@gmail.com>
2015-08-04 12:45:07 +0600

    04AUGUST2015 12:45

---
+++ src/main/java/test/maven/maven1/test/maven/module4/App4.java
@@ -0,0 +1,11 @@
+package test.maven.maven1.test.maven.module4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App4 {
+	public String test4(String test4){
+	return "This is TEST = 4 :: " + test4;	
+	}
+}
